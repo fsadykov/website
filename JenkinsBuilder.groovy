46909239679e1b6783c06fb2fdc10b43384d21e1
fsadykov <sadykovfarkhod@gmail.com>
2022-11-14 18:46:54 -0600

    Ready to deploy stage env

---
+++ JenkinsBuilder.groovy
@@ -0,0 +1,10 @@
+// Using the existing Jenkins Global Library 
+@Library('CommonLib@master') _
+
+
+// Defining the main build pipeline
+def common = new com.lib.JenkinsCommonDockerBuildPipeline()
+
+
+// Calling the function within the main pipeline 
+common.runPipeline()
